aac9bb50e81dfea572a0746e70cb4308d913a745
sahba atarodi <sahba.atarodi@gmail.com>
2023-12-18 14:40:38 -0500

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Linq
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string> { "The legend of Zelda", "Tetris", "Pac-Man", "Minecraft" };
+            var orderedGames = videoGames.OrderBy(game => game.Length);
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
